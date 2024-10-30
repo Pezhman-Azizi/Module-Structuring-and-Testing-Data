1b8491999a9eb076cce57919642b952867986052
Pezhman-Azizi <80008463+Pezhman-Azizi@users.noreply.github.com>
2024-10-30 23:04:54 +0000

    Explained how = works to update count by adding 1 to itself

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -4,3 +4,11 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+/////////////////////////////////////////////// ANSWER /////////////////////////////////////////////////
+// In this task, we have the `+` operator and the `=` assignment operator. The assignment operator takes
+// a value on the right side and assigns it to the variable on the left side.
+// Initially, the `count` variable is set to `0` in line #1.
+// In line #3, we take the current value of `count` (which is 0), add 1 to it using the `+` operator,
+// and then assign the result (1) back to the `count` variable.
+// This increments the `count` variable by 1, so `count` is now 1.
