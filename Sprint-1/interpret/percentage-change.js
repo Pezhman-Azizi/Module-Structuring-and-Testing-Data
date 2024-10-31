825cce821b79e52161aaf1d4e9ce17ce4b5f16a8
Pezhman-Azizi <80008463+Pezhman-Azizi@users.noreply.github.com>
2024-10-31 23:47:21 +0000

    interpret-percentageTask

--- Sprint-1/interpret/percentage-change.js
+++ Sprint-1/interpret/percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",",""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -20,3 +20,30 @@ console.log(`The percentage change is ${percentageChange}`);
 // d) Identify all the lines that are variable declarations
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+
+
+//----------------------------------------------------------ANSWER-------------------------------------------------------------------
+
+// -----------------a) Function-calls:
+// 1. carPrice.replaceAll(",", "") (in line 3)
+// 2. Number(carPrice.replaceAll(",", "")) (in line 3)
+// 3. priceAfterOneYear.replaceAll(",", "") (in line 4)
+// 4. Number(priceAfterOneYear.replaceAll(",", "")) (in line 4)
+// 5. console.log(...) (in line 8)
+
+// -----------------b) where is the error?:
+// in line 5.
+// Reason: It is because `.replaceAll takes two arguments with a comma in between, We are missing that comma in between.
+// How to fix: by adding a comma between two arguments.
+
+// -----------------c) Variable reassignment
+// line 4 and 5 are the variable reassignments
+//
+// -----------------d) Variable declaration
+// lines 1, 2, 7 and 8
+//
+// -----------------e) Variable declaration
+// The purpose of this expression is to take a string with commas (like "10,000"), remove the commas,
+// and then convert it to a numeric type. This makes it possible to use carPrice in mathematical operations,
+// as it is now a number without any formatting characters.
