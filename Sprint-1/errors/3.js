9ad100b3760ccbf15c85780800269520c5db15a1
Pezhman-Azizi <80008463+Pezhman-Azizi@users.noreply.github.com>
2024-10-31 22:10:24 +0000

    Fixed code to extract last 4 digits by converting number to string

--- Sprint-1/errors/3.js
+++ Sprint-1/errors/3.js
@@ -1,5 +1,7 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const last4Digits = cardNumber.toString().slice(-4);
+console.log(last4Digits);
+
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +9,7 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+// The code is NOT running because `.slice` is only applied to strings.
+// It only applies to strings
+// What needs to be done is to at first turn the `cardNumber` to string and then apply the `.slice(-4)` on it.
