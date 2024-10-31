ec8f1b4798e2995fa8835077cc100cfda600eb2d
Pezhman-Azizi <80008463+Pezhman-Azizi@users.noreply.github.com>
2024-10-31 00:04:11 +0000

    Got initials by taking the first letter of each name

--- Sprint-1/exercises/initials.js
+++ Sprint-1/exercises/initials.js
@@ -4,3 +4,8 @@ let lastName = "Johnson";
 
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
+
+//we can get the first character of a variable (with the string value) by `.charAt(0)` and zero indicates the
+// first index of the variable giving us the first character. 
+const initials = firstName.charAt(0) + middleName.charAt(0) + lastName.charAt(0)
+console.log(initials);
