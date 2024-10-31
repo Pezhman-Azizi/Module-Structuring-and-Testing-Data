59693eb2a42e5ff29c9d8718ab86533517e4b180
Pezhman-Azizi <80008463+Pezhman-Azizi@users.noreply.github.com>
2024-10-31 21:41:08 +0000

    Defined a variable before using it afterwards.

--- Sprint-1/errors/2.js
+++ Sprint-1/errors/2.js
@@ -1,5 +1,8 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+// The error is because in line 5, the variable `cityOfBirth` is now defined prior to using it in a console.log.
+
