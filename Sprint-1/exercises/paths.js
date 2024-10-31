72f195dac3fbd4cce144d38a34d05b695c7372d9
Pezhman-Azizi <80008463+Pezhman-Azizi@users.noreply.github.com>
2024-10-31 00:35:24 +0000

    Grabbed the directory and file type from the file path

--- Sprint-1/exercises/paths.js
+++ Sprint-1/exercises/paths.js
@@ -16,3 +16,14 @@ console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
+
+// `dir` part of a file-path is the directory structure excluding the file itself. So we need to reach till the last forward slash.
+const dir = filePath.slice(0, lastSlashIndex);
+
+// Extract the `ext` part (file extension) by finding the last dot in the base
+const lastDotIndex = base.lastIndexOf(".");
+const ext = base.slice(lastDotIndex);
+
+//logging the variables to the console
+console.log(`The dir part of ${filePath} is ${dir}`);
+console.log(`The ext part of ${filePath} is ${ext}`);
