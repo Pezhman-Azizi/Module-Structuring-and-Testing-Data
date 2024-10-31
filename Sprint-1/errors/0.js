edfb33c0f9be0ee264add343e2ef85a1c1a73408
Pezhman-Azizi <80008463+Pezhman-Azizi@users.noreply.github.com>
2024-10-31 20:41:06 +0000

    Explained how commenting works in JavaScript.

--- Sprint-1/errors/0.js
+++ Sprint-1/errors/0.js
@@ -1,2 +1,7 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+// This is just an instruction for the first activity - but it is just for human consumption
+// We don't want the computer to run these 2 lines - how can we solve this problem?
+
+// We can take an approach called `Commenting`. In different programming languages, there are different ways to solve this.
+// And it is just a matter of Syntax. In case of JavaScript for Mac we can do `command + /`
+// it means the line is not executed and it is just for sake of reading it.
+// So in the case of this exercise I want to comment out the 2 lines (line1 and line2) by selecting both lines and pressing `command + /`
