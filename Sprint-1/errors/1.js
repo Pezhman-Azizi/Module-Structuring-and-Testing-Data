87e7602cc2f7e1d3770c20f0f04d3867404bf9f3
Pezhman-Azizi <80008463+Pezhman-Azizi@users.noreply.github.com>
2024-10-31 21:26:57 +0000

    used let keyword to be able to overwrite the variable's value

--- Sprint-1/errors/1.js
+++ Sprint-1/errors/1.js
@@ -1,4 +1,9 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
 age = age + 1;
+console.log(age);
+
+// We need to change the line 3. And define the variable with `let`.
+// Rather than using `const`.
+// It gives the chance to change the values and do the over ride the variable. 
