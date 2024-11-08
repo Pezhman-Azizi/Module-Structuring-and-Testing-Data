904b0f2dd060af80e03d257089c0d2f4bb594838
Pezhman-Azizi <80008463+Pezhman-Azizi@users.noreply.github.com>
2024-11-08 23:06:20 +0000

    implemented a function block

--- Sprint-2/implement/to-pounds.js
+++ Sprint-2/implement/to-pounds.js
@@ -4,3 +4,25 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+
+const penceString = "399p";
+
+function toPounds(penceString){
+  // Remove the trailing 'p' from the input string
+  const penceStringWithoutTrailingP = penceString.substring(0,penceString.length - 1);
+
+  // .padStart ensures it has at least three characters which means it makes sure that it has three digits.
+  // And if it was less then three digits it adds 0 in the beginning of the number.
+  const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+
+  // Extract the pounds and pence parts
+  const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2);
+  const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
+
+  return (`£${pounds}.${pence}`);
+}
+
+console.log(toPounds("30p"));
+
+
