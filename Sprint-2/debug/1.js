3726f07791f543a400c3c8cca04509e77a3a15b0
Pezhman-Azizi <80008463+Pezhman-Azizi@users.noreply.github.com>
2024-11-08 20:11:10 +0000

    debug-1.js

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,8 +1,16 @@
-// Predict and explain first...
+// 1. Predict and explain first...
+//  In line 6, the `a+b` is not placed correctly I think.
+//  After running with node, it gives my an undefined output. The reason is that, right after the return
+//  the function stops. The calculation in line 8 should be in line 7 as follows
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+// I realized that the out put message also should be dynamic. So in line 10 and 11 i have defined
+// variables which makes the text by console.log more dynamic. This way, each time we give arguments to call the function
+// 
+
+a= 0;
+b= 32;
+console.log(`The sum of ${a} and ${b} is ${sum(10, 32)}`);
