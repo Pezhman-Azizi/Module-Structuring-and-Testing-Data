5b1bf08cef0b081f10e4de31cf61993e8442b6c5
Pezhman-Azizi <80008463+Pezhman-Azizi@users.noreply.github.com>
2024-11-08 15:29:34 +0000

    1.js Fixed duplicate declaration of decimalNumber in convertToPercentage function

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -1,13 +1,30 @@
-// Predict and explain first...
+// 1. Predict and explain first...
+  // In this code, decimalNumber is used twice: once as an input to the function (a parameter) and again
+  // as a const inside the function.
+  // JavaScript doesn’t allow us to use the same name twice like this, especially when using const inside the function.
 
-// Why will an error occur when this program runs?
-// Try playing computer with the example to work out what is going on
+// 2. Why will an error occur when this program runs?
+  // SyntaxError: Identifier 'decimalNumber' has already been declared.
+  // Reason: Since const makes a variable constant (unchangeable) and unique, JavaScript doesn’t let us
+  // create it again with the same name as the input to the function.
+
+
+// 3. Try playing computer with the example to work out what is going on
+  // Removed `const decimalNumber = 0.5;` from the code
+  // In line 28, replaced the `decimalNumber` with `convertToPercentage()` and put a decimalNumber as an argument.
+  // This way function is called and the argument is passes and as a result the percentage is returned and
+  //  finally in line 30, the result is returned in the console. 
+
+
+
+
+
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage(0.3));
