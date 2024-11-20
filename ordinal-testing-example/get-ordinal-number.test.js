cb7e72f7bfa31eee9ff776940a942863d177131c
Pezhman-Azizi <80008463+Pezhman-Azizi@users.noreply.github.com>
2024-11-20 22:30:53 +0000

    working out the test in prep section on the module

---
+++ ordinal-testing-example/get-ordinal-number.test.js
@@ -0,0 +1,8 @@
+function getOrdinalNumber() {
+  return "1st";
+}
+test("works for any number ending in 1", function() {
+  expect(getOrdinalNumber(1)).toEqual("1st");
+  expect(getOrdinalNumber(11)).toBe("11th");
+  expect(getOrdinalNumber(21)).toBe("21st");
+});
